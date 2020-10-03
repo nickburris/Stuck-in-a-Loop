8742eff5458db5b02e0334318dee2857d3dadbd2
Nick Burris <nickburris13@gmail.com>
2020-10-02 22:11:30 -0400

    Add mouse event queue

--- game.js
+++ game.js
@@ -1,9 +1,16 @@
 class Game {
   constructor() {
+    this.eventQueue = [];
     this.canvas = document.createElement("canvas");
+    this.canvas.addEventListener("click", this.onclick.bind(this));
     document.getElementById("container").appendChild(this.canvas);
 
     this.needsDraw = true;
+  }
+
+  // Add a click event to the event queue
+  onclick(event) {
+    this.eventQueue.push(event);
   }
 
   // Draw the game in its current state
@@ -13,7 +20,13 @@ class Game {
 
   // Update game state then draw and request another frame
   update() {
-    // TODO game logic
+    // Handle events
+    while (this.eventQueue.length) {
+      var e = this.eventQueue.shift();
+      if (e instanceof MouseEvent) {
+        console.log("x: " + e.x + ", y: " + e.y);
+      }
+    }
 
     // Draw and request another frame
     this.draw();
